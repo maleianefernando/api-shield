23549f6d1e6f2283ef4b345387ecca507775e571
Fernando Maleiane <maleianefernando3@gmail.com>
2026-04-28 15:05:48 +0200

    feat: add nonce validation with exceptions throwing

--- src/Services/NonceService.php
+++ src/Services/NonceService.php
@@ -3,20 +3,30 @@
 namespace Maleianefernando\ApiShield\Services;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+
 // use Illuminate\Support\Str;
 class NonceService
 {
+    private $ttl = null;
     public function __construct()
     {
+        $this->ttl = config('apishield.noncettl');
     }
 
     public function persist(string $nonce)
     {
-        return Cache::add($nonce, true, 300);
+        throw_if(! Cache::add($nonce, true, $this->ttl), \Exception::class, "Please be sure that the given nonce value was never used.");
+        
+        return true;
     }
 
-    public function exists(array $nonce)
+    public function exists(string $nonce)
     {
-        return Cache::get($nonce);
+        if(Cache::get($nonce)){
+            return true;
+        }
+
+        return false;
     }
 }
