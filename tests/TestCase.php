4e26b3b323ae9ec8d5b55d72c62ddd4420ac66e0
Fernando Maleiane <maleianefernando3@gmail.com>
2026-04-28 15:47:03 +0200

    config: set fake environment variables just for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tests;
 
+use Illuminate\Contracts\Config\Repository; 
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app) 
@@ -11,6 +13,24 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageAliases($app)
     {
         return parent::getPackageAliases($app);
+    }
+
+    protected function defineEnvironment($app)
+    {
+        tap($app['config'], function(Repository $config) {
+            $config->set('apishield.secret', 'z@LZdMeyJbcDQxauD-4+qRMWMGa8Aqgx');
+            $config->set('apishield.noncettl', 60);
+            $config->set('apishield.timestamplimit', 60);
+
+            $config->set('cache.default', 'redis'); // Tested: 'file' | 'redis'
+
+            // $config->set('database.connections.testbench', [ 
+            //     'driver'   => 'mysql', 
+            //     'database' => 'testbench',
+            //     'prefix'   => '', 
+            // ]);
+        });
+        // return parent::defineEnvironment($app);
     }
 
     protected function setUp(): void
