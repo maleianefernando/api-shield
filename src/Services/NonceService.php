679e9fa0e2399d19eaf2b23f0bcd7d7d2b9bb974
Fernando Maleiane <maleianefernando3@gmail.com>
2026-04-29 00:45:17 +0200

    refactor

--- src/Services/NonceService.php
+++ src/Services/NonceService.php
@@ -3,9 +3,7 @@
 namespace Maleianefernando\ApiShield\Services;
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Str;
 
-// use Illuminate\Support\Str;
 class NonceService
 {
     private $ttl = null, $prefix = null;
@@ -17,7 +15,6 @@ class NonceService
 
     public function persist(string $nonce)
     {
-        dump($this->getPatternedNonce($nonce));
         throw_if(! Cache::add($this->getPatternedNonce($nonce), true, $this->ttl), \Exception::class, "Please be sure that the given nonce value was never used.");
         
         return true;
