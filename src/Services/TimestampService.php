a82126c36db3e3d904cf4284f0fd7ca07c80c576
Fernando Maleiane <maleianefernando3@gmail.com>
2026-04-28 15:04:09 +0200

    feat: add timestamp validation

--- src/Services/TimestampService.php
+++ src/Services/TimestampService.php
@@ -2,17 +2,28 @@
 
 namespace Maleianefernando\ApiShield\Services;
 
+use DateTime;
 use Illuminate\Support\Facades\Cache;
 // use Illuminate\Support\Str;
 class TimestampService
 {
+    private $limit = null;
+    
     public function __construct()
     {
+        $this->limit = config('apishield.timestamplimit');
     }
-
+        
     public function isValid(int $timestamp): bool
     {
-        if(!is_numeric($timestamp) || (abs(time() - $timestamp) > 300)) {
+        throw_if(!is_numeric($timestamp), \Exception::class, "Please be sure that this is a valid timestamp.");
+        
+        throw_if(!DateTime::createFromFormat('U', $timestamp), \Exception::class, "Please be sure that this is a valid timestamp.");
+
+        // sleep(60);
+        $diff = abs(time() - $timestamp);
+
+        if($diff > $this->limit) {
             return false;
         }
 
