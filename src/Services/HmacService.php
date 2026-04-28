5bcb846947ddc74b61eed9a073c227ae7f16e0b1
Fernando Maleiane <maleianefernando3@gmail.com>
2026-04-28 15:18:48 +0200

    feat: add hmac validation with exceptions throwing

--- src/Services/HmacService.php
+++ src/Services/HmacService.php
@@ -4,29 +4,23 @@ namespace Maleianefernando\ApiShield\Services;
 
 class HmacService
 {
-    private $secret = '';
-    public function __construct(string $secret)
+    private $secret = null;
+    public function __construct()
     {
-        $this->setSecret($secret);
+        $this->secret = config('apishield.secret');
     }
 
-    public function setSecret(string $secret)
+    public function write(string $data): string
     {
-        $this->secret = $secret;
-    }
+        throw_if(!isset($this->secret), \Exception::class, "Hmac secret not set.");
 
-    public function getSecret(): string
-    {
-        return $this->secret;
-    }
-
-    public function write(string $pattern): string
-    {
-        return hash_hmac('sha256', $pattern, $this->secret);
+        return hash_hmac('sha256', $data, $this->secret);
     }
 
     public function check(array $hmac): bool
     {
+        throw_if(!isset($hmac[0]) && !isset($hmac[1]), \Exception::class, "Invalid hmac check args.");
+
         return hash_equals($hmac[0], $hmac[1]);
     }
 }
